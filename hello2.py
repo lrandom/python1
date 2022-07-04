e2c27c071b8845956fc3758f0a511cb0296b00c0
Nguyen Thanh Luan <luann4099@gmail.com>
2022-07-04 20:38:56 +0700

    init session 1

---
+++ hello2.py
@@ -0,0 +1,11 @@
+def print_hi():
+    print("Hello")
+    print("Hello World")
+    print("Hi")
+
+
+print_hi()
+
+print_hi()
+
+print_hi()
---
+++ naming_style.py
@@ -0,0 +1,19 @@
+# pascal
+# camelCase
+# snake_case = under_score
+# kebab-case
+
+# pascal
+MyName = "Pascal"  # thuong duoc su dung de dat ten cho lop: python
+
+# camelCase
+myFullName = "Nguyen Thanh Luan"  # thuong duoc su dung de dat ten cho bien, ham: php,java,c++, javascript
+
+# snake_case = under_score
+my_full_name = "Nguyen Thanh Luan"  # thuong duoc su dung de dat ten cho bien, ham: python, java, php, c++, js
+
+# kebab-case
+# my-full-name = "Luan" #0 su dung trong dat ten bien, dat ten cho lop(class) trong CSS (FE)
+
+# age = 20
+# age2 = 30
